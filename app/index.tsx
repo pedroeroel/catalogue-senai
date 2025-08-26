ce01efe9c2b5ad1f14df46493a403aa12f301337
pedroeroel <pedroedroel@gmail.com>
2025-08-26 14:14:35 -0300

    first

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Image } from 'expo-image';
+import { View, Text } from 'react-native';
+import CatalogueScreen from '../components/catalogueScreen';
+
+export default function HomeScreen() {
+  return (
+<CatalogueScreen />
+  );
+}
